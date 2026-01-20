c0df9018202c9e26e28e5d853a2399137b113b8c
VillainAlexandre <villainalexandre89@gmail.com>
2026-01-20 11:50:07 +0100

    chantier placer automatiquement par date

--- src/Repository/ChantierRepository.php
+++ src/Repository/ChantierRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Chantier;
+use App\Entity\Statut;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,15 +17,63 @@ class ChantierRepository extends ServiceEntityRepository
         parent::__construct($registry, Chantier::class);
     }
 
+    public function updateStatutsByDates(): void
+    {
+        $em = $this->getEntityManager();
+        $today = new \DateTimeImmutable('today');
+
+        $chantiers = $this->createQueryBuilder('c')
+            ->leftJoin('c.statut', 's')
+            ->addSelect('s')
+            ->where('c.archive = 0')
+            ->getQuery()
+            ->getResult();
+
+        foreach ($chantiers as $chantier) {
+
+        
+           
+            if ($chantier->getDateFin() !== null && $chantier->getDateFin() < $today) {
+                $statutId = 3;
+            }
+          
+            elseif (
+                $chantier->getDateDemarrage() !== null &&
+                $chantier->getDateDemarrage() <= $today
+            ) {
+                $statutId = 1;
+            }
+           
+            else {
+                $statutId = 2;
+            }
+
+            if ($chantier->getStatut()?->getId() !== $statutId) {
+                $chantier->setStatut(
+                    $em->getReference(\App\Entity\Statut::class, $statutId)
+                );
+            }
+        }
+
+        $em->flush();
+    }
+
+
+    /**
+     * Récupération des chantiers par statut
+     */
     public function findChantiersByStatutId(int $statutId): array
     {
         return $this->createQueryBuilder('c')
             ->innerJoin('c.statut', 's')
             ->where('s.id = :statutId')
+            ->andWhere('c.archive = 0')
             ->setParameter('statutId', $statutId)
+            ->orderBy('c.dateDemarrage', 'ASC')
             ->getQuery()
             ->getResult();
     }
+
 
     //    /**
     //     * @return Chantier[] Returns an array of Chantier objects
